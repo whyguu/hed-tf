cf649b8f8cb34d1fbc3c9aea47ef2ae5cc08aab3
whyguu <whyguu@sina.com>
2018-01-18 10:57:12 +0800

    add pull docker

--- tf-docker-requirements.sh
+++ tf-docker-requirements.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+docker pull tensorflow/tensorflow:latest-gpu-py3
+nvidia-docker run -ti --name=ygw-tf -p 8080:8888 -p 2121:22 -p 6666:6006 -v $pwd/docker:/workspace -w /workspace tensorflow/tensorflow:latest-gpu-py3 bash
 apt-get update
 pip3 install pyyaml
 pip3 install opencv-python>=3.0
