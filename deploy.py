3bc9313cc84895910bc3f0082127d5255fc57ef4
whyguu <whyguu@sina.com>
2018-04-25 15:45:50 +0800

    update

--- deploy.py
+++ deploy.py
@@ -32,6 +32,28 @@ def sess_config(args=None):
 
 
 def img_pre_process(img, **kwargs):
+    def stretch(bands, lower_percent=2, higher_percent=98, bits=8):
+        if bits not in [8, 16]:
+            print('error ! dest image must be 8bit or 16bits !')
+            return
+        out = np.zeros_like(bands, dtype=np.float32)
+        n = bands.shape[2]
+        for i in range(n):
+            a = 0
+            b = 1
+            c = np.percentile(bands[:, :, i], lower_percent)
+            d = np.percentile(bands[:, :, i], higher_percent)
+            if d-c == 0:
+                out[:, :, i] = 0
+                continue
+            t = a + (bands[:, :, i] - c) * (b - a) / (d - c)
+            out[:, :, i] = np.clip(t, a, b)
+        if bits == 8:
+            return out.astype(np.float32)*255
+        else:
+            return np.uint16(out.astype(np.float32)*65535)
+
+    img = stretch(img)
     img -= kwargs['mean']
     return img
 
@@ -45,6 +67,8 @@ def predict_big_map(img_path, out_shape=(448, 448), inner_shape=(224, 224), out_
     :param pred_fun: forward model
     :return: predicted image
     """
+    make_video = True
+
     image = cv2.imread(img_path, )
     if len(image.shape) == 2:
         image = np.expand_dims(image, axis=-1)
@@ -56,35 +80,71 @@ def predict_big_map(img_path, out_shape=(448, 448), inner_shape=(224, 224), out_
     pd_bm_h = (out_shape[0]-pd_up_h) - (image.shape[0] % inner_shape[0])
     pd_rt_w = (out_shape[1]-pd_lf_w) - (image.shape[1] % inner_shape[1])
 
-    it_h = np.int64(np.ceil(image.shape[0] / inner_shape[0]))
-    it_w = np.int64(np.ceil(image.shape[1] / inner_shape[1]))
+    it_h = np.int64(np.ceil(1.0*image.shape[0] / inner_shape[0]))
+    it_w = np.int64(np.ceil(1.0*image.shape[1] / inner_shape[1]))
 
-    image = np.pad(image, ((pd_up_h, pd_bm_h), (pd_lf_w, pd_rt_w), (0, 0)), mode='reflect').astype(np.float32)  # the image is default a color one
-    print(image.shape)
+    image_pd = np.pad(image, ((pd_up_h, pd_bm_h), (pd_lf_w, pd_rt_w), (0, 0)), mode='reflect').astype(np.float32)  # the image is default a color one
+    print(image_pd.shape)
     print((pd_up_h, pd_bm_h), (pd_lf_w, pd_rt_w))
     gc.collect()
 
     tp1 = np.array(inner_shape[0] - ori_shape[0] % inner_shape[0])
     tp2 = np.array(inner_shape[1] - ori_shape[1] % inner_shape[1])
+    if ori_shape[0] % inner_shape[0] == 0:
+        tp1 = 0
+    if ori_shape[1] % inner_shape[0] == 0:
+        tp2 = 0
+
     out_img = np.zeros((ori_shape[0]+tp1, ori_shape[1]+tp2, out_channel), np.float32)
 
+    # video config #################################
+    if make_video:
+        fps = 24  # 视频帧率
+        wd = 1360
+        ht = int(1360*out_img.shape[0]/out_img.shape[1])
+        # haha = np.zeros((ht, wd, 3), np.uint8)
+        haha = cv2.resize(np.pad(image, ((0, tp1), (0, tp2), (0, 0)), mode='reflect'), (wd, ht), interpolation=cv2.INTER_LINEAR)
+        video_writer = cv2.VideoWriter('./data/s2.avi',
+                                       cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps,
+                                       (wd, ht))   # isColor=False? (1360,480)为视频大小
+    image = None  # release memory
+    # main loop
     for ith in range(0, it_h):
         h_start = ith * inner_shape[0]
         count = 1
         for itw in range(0, it_w):
             w_start = itw*inner_shape[1]
-            tp_img = image[h_start:h_start+out_shape[0], w_start:w_start+out_shape[1], :]
+            tp_img = image_pd[h_start:h_start+out_shape[0], w_start:w_start+out_shape[1], :]
 
+            # image pre-process
             tp_img = img_pre_process(tp_img.copy(), **kwargs)
             # print('tp_img', tp_img.shape)
 
             tp_out = pred_fun(tp_img[np.newaxis, :])
             tp_out = np.squeeze(tp_out, axis=0)
+
+            # image post-process
+            # tp_out = post-process
+
             out_img[h_start:h_start+inner_shape[0], w_start:w_start+inner_shape[1], :] = tp_out[pd_up_h:pd_up_h+inner_shape[0], pd_lf_w:pd_lf_w+inner_shape[1], :]
 
-            print('hahahaha!', h_start, w_start, count)
+            # write video ##########################
+            if make_video:
+                tp = cv2.resize(out_img[:, :, 0], (wd, ht), interpolation=cv2.INTER_LINEAR)
+                # print(np.unique(tp))
+                # xixi = np.uint8((tp > 0.5)*255)
+                xixi = tp > 1e-5
+                mimi = np.uint8(tp[xixi] * 255)
+                haha[xixi, 0] = mimi
+                haha[xixi, 1] = mimi
+                haha[xixi, 2] = mimi
+                video_writer.write(haha)
+
+            print('haha!', h_start, w_start, count)
             count += 1
-    return out_img
+    if make_video:
+        video_writer.release()
+    return out_img[0:ori_shape[0], 0:ori_shape[1], :]
 
 
 if __name__ == '__main__':
@@ -108,17 +168,17 @@ if __name__ == '__main__':
              tf.sigmoid(hed_class.side4),
              tf.sigmoid(hed_class.side5),
              tf.sigmoid(hed_class.fused_side)]
-    sides = tf.add_n(sides) / len(sides)
+    sides = 1.0*tf.add_n(sides) / len(sides)
     sess = tf.Session(config=config)
     saver = tf.train.Saver()
     # load weights
-    saver.restore(sess, cfg['model_weights_path'] + '/vgg16_hed-88')
+    saver.restore(sess, cfg['model_weights_path'] + 'tb_bak/vgg16_hed-450')
 
     output_img = predict_big_map(img_path=path, out_shape=(448, 448), inner_shape=(224, 224), out_channel=1,
                                  pred_fun=(lambda ipt: sess.run(sides, feed_dict={hed_class.x: ipt})), mean=cfg['mean'])
     output_img = np.squeeze((output_img*255).astype(np.uint8))
-    cv2.imwrite('gray_img.png', output_img)
-    cv2.imwrite('black_img.png', 255*(output_img > 127))
+    cv2.imwrite('./data/tb_gray_img.png', output_img)
+    cv2.imwrite('./data/tb_black_img.png', 255*(output_img > 127))
     sess.close()
 
 
